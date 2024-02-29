ed5eb765c7a23ffe2e040ddca6c4132154ec96c7
aluno <vinicius.s.silveira.2015@gmail.com>
2024-02-29 20:12:06 -0300

    IfElse

---
+++ src/main/java/com/mycompany/exerciciosifelse/ExerciciosIfelse.java
@@ -0,0 +1,38 @@
+package com.mycompany.exerciciosifelse;
+
+import java.util.Scanner;
+
+
+public class ExerciciosIfelse {
+
+    public static void main(String[] args) {
+
+Scanner teclado2 = new Scanner(System.in);
+
+ int numero;
+ 
+        System.out.println(" Calcule ");
+        numero = teclado2.nextInt();
+        
+        if (numero < 0){  //Se o numero for menor que zero irá ser negativo
+        System.out.println("Este número é negativo"); 
+    } else {             //Se não for menor que zero, é positivo.
+            System.out.println("Este número é positivo");
+            
+            }
+   
+    }
+} 
+        
+
+
+                
+
+    
+
+    
+
+    
+
+
+    
